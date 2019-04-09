d38039cf48ee0d79dbc1c1fd76ff1eaa8e495fb9
jaamp <jtowery@uw.edu>
2019-04-08 21:35:35 -0700

    adding assignment1

--- blank.js
+++ blank.js
@@ -1,1 +1,29 @@
-console.log('hi');
+for (let i = 0; i < 10; i++) {
+
+var first = Math.ceil(Math.random() * 10);
+var second = Math.ceil(Math.random() * 10);
+var operand = Math.ceil(Math.random() * 4);
+var results;
+
+switch (operand) {
+    case (1): 
+        results = first + second;
+        console.log(first+ ' + ' +second+ ' = ' +results);
+    break;
+    case (2): 
+        results = first - second;
+        console.log(first+ ' - ' +second+ ' = ' +results);
+    break;
+    case (3): 
+        results = first * second;
+        console.log(first+ ' * ' +second+ ' = ' +results);
+    break;
+    case (4): 
+        results = first / second;
+        console.log(first+ ' / ' +second+ ' = ' +results);
+    break;
+    default: 
+        console.log('Nothing to see here');
+    break;
+};
+}
