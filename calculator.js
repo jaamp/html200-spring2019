02dd2023b44bde6b5b0168a8022440ae3e74997b
jaamp <jtowery@uw.edu>
2019-04-08 22:01:18 -0700

    final edit

--- calculator.js
+++ calculator.js
@@ -1,29 +1,20 @@
-for (let i = 0; i < 10; i++) {
-
-var first = Math.ceil(Math.random() * 10);
-var second = Math.ceil(Math.random() * 10);
-var operand = Math.ceil(Math.random() * 4);
 var results;
+for (let i = 0; i < 3; i++) {
 
-switch (operand) {
-    case (1): 
-        results = first + second;
-        console.log(first+ ' + ' +second+ ' = ' +results);
-    break;
-    case (2): 
-        results = first - second;
-        console.log(first+ ' - ' +second+ ' = ' +results);
-    break;
-    case (3): 
-        results = first * second;
-        console.log(first+ ' * ' +second+ ' = ' +results);
-    break;
-    case (4): 
-        results = first / second;
-        console.log(first+ ' / ' +second+ ' = ' +results);
-    break;
-    default: 
-        console.log('Nothing to see here');
-    break;
-};
+    console.log('Round: ' + (i + 1));
+
+    var first = Math.ceil(Math.random() * 10);
+    var second = Math.ceil(Math.random() * 10);
+
+            results = first + second;
+            console.log(first+ ' + ' +second+ ' = ' +results);
+
+            results = first - second;
+            console.log(first+ ' - ' +second+ ' = ' +results);
+    
+            results = first * second;
+            console.log(first+ ' * ' +second+ ' = ' +results);
+
+            results = first / second;
+            console.log(first+ ' / ' +second+ ' = ' +results);
 }
